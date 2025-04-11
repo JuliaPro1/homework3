b50fcb25d90a1fd665d00f30e6c3d7661058608f
Julia <julpkolp@gmail.com>
2025-04-11 11:09:25 +0300

    FillStudentRegistrationForm changes2 were added

--- src/test/java/FillStudentRegistrationForm.java
+++ src/test/java/FillStudentRegistrationForm.java
@@ -13,7 +13,7 @@ public class FillStudentRegistrationForm {
 
     @BeforeAll
     static void beforeAll() {
-        //Configuration.browserSize = "1920x1080";
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
